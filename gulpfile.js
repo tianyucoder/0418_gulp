a4e2b25fdf9a45ab1b99122f46834fcec0476836
tianyu <0318@qq.com>
2019-07-30 17:05:13 +0800

    update

--- gulpfile.js
+++ gulpfile.js
@@ -12,6 +12,17 @@ const browserify = require('gulp-browserify');
 const rename = require('gulp-rename');
 //引入gulp-uglify
 const uglify = require('gulp-uglify')
+//引入gulp-less
+const less = require('gulp-less');
+const LessAutoprefix = require('less-plugin-autoprefix')
+const autoprefix = new LessAutoprefix({ browsers: ['last 2 versions'] });
+const concat = require('gulp-concat');
+const cssmin = require('gulp-cssmin');
+const htmlmin = require('gulp-htmlmin');
+// const imagemin = require('gulp-imagemin');
+const livereload = require('gulp-livereload');
+const connect = require('gulp-connect');
+const opn = require('opn');
 
 //语法检查
 gulp.task('jshint', function() {
@@ -20,7 +31,8 @@ gulp.task('jshint', function() {
     .pipe(jshint({
       esversion: 6
     }))
-    .pipe(jshint.reporter(stylish));
+    .pipe(jshint.reporter(stylish))
+    .pipe(livereload());
 });
 
 //语法转换
@@ -30,6 +42,7 @@ gulp.task('babel', () => {
       presets: ['@babel/env']
     }))
     .pipe(gulp.dest('build/js'))//输出到指定目录
+    .pipe(livereload());
 });
 
 //模块化语法转换
@@ -38,6 +51,7 @@ gulp.task('browserify', function() {
     .pipe(browserify())//将CommonJs语法转换为浏览器能识别的语法
     .pipe(rename('built.js'))//为了防止冲突将文件重命名
     .pipe(gulp.dest('build/js/'))//输出到指定位置
+    .pipe(livereload());
 });
 
 //压缩js
@@ -46,11 +60,76 @@ gulp.task('uglify', function () {
     .pipe(uglify())  //压缩js
     .pipe(rename('dist.min.js'))
     .pipe(gulp.dest('dist/js'))
+    .pipe(livereload());
 });
 
+//编译less且扩展前缀
+gulp.task('less', function () {
+  return gulp.src('./src/less/*.less')
+    .pipe(less({
+      plugins: [autoprefix]//自动扩展前缀
+    }))
+    .pipe(gulp.dest('./build/css'))
+    .pipe(livereload());
+});
+
+gulp.task('concat', function() {
+  return gulp.src('./build/css/*.css')
+    .pipe(concat('built.css'))
+    .pipe(gulp.dest('./build/css'))
+    .pipe(livereload());
+});
+
+gulp.task('cssmin', function () {
+  return gulp.src('build/css/built.css')
+    .pipe(cssmin())
+    .pipe(rename('dist.min.css'))
+    .pipe(gulp.dest('dist/css'))
+    .pipe(livereload());
+});
+
+gulp.task('htmlmin', () => {
+  return gulp.src('src/index.html')
+    .pipe(htmlmin({
+      collapseWhitespace: true ,//去除空格
+      removeComments:true //去除注释
+    }))
+    .pipe(gulp.dest('dist'))
+    .pipe(livereload());
+});
+
+gulp.task('imagemin', function () {
+  return gulp.src('./src/img/*.{png,jpg,gif,ico}')
+    .pipe(imagemin({
+      optimizationLevel: 3, //类型：Number  默认：3  取值范围：0-7（优化等级）
+      progressive: true, //类型：Boolean 默认：false 无损压缩jpg图片
+      interlaced: true, //类型：Boolean 默认：false 隔行扫描gif进行渲染
+      multipass: true //类型：Boolean 默认：false 多次优化svg直到完全优化
+    }))
+    .pipe(gulp.dest('dist/img'))
+    .pipe(livereload());
+});
+
+//自动执行任务，编译代码
+gulp.task('watch', function() {
+  //1. 在所有可能要执行任务后面加上 .pipe(livereload());
+  livereload.listen();
+  //通过自己服务器打开项目，自动刷新
+  connect.server({
+    root: 'dist',
+    port: 3001,
+    livereload: true  // 自动刷新
+  });
+  //自动打开浏览器
+  opn('http://localhost:3001/index.html');
+  //监视指定文件（第一个参数），一旦文件发生变化，就自动执行后面的任务（第二个参数）
+  gulp.watch('src/less/*.less', gulp.series(['less','concat','cssmin']));
+  gulp.watch('./src/js/*.js', gulp.series(['jshint','babel','browserify','uglify']));
+  gulp.watch('./src/index.html', gulp.series('htmlmin'));
+});
 
 //配置默认任务
-gulp.task('default', gulp.series('jshint', 'babel', 'browserify','uglify'))
+gulp.task('default', gulp.series('jshint', 'babel', 'browserify','uglify','less','concat','cssmin','htmlmin','watch'))
 
 
 
